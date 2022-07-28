82e218b95655e99162e2cea5e462f863715b2ca7
Mike Zhang <mikezhang@AFFINITYMAC-153.local>
2022-07-28 16:18:44 +0800

    first push

---
+++ src/components/sections/About.js
@@ -0,0 +1,98 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { Button } from "../Button";
+import Carousel from "../Carousel";
+import { dark } from "../../styles/Themes";
+
+const Section = styled.section`
+  min-height: 100vh;
+  width: 100%;
+  background-color: ${(props) => props.theme.text};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+const Container = styled.div`
+  width: 75%;
+  min-height: 88vh;
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h2`
+  font-size: ${(props) => props.theme.fontxxl};
+  text-transform: capitalize;
+  width: 80%;
+  color: ${(props) => props.theme.body};
+  align-self: flex-start;
+  margin: 0 auto;
+`;
+
+const SubText = styled.p`
+  font-size: ${(props) => props.theme.fontlg};
+  text-transform: capitalize;
+  width: 80%;
+  color: ${(props) => props.theme.body};
+  align-self: flex-start;
+  margin: 1rem auto;
+  font-weight: 400;
+`;
+
+const SubTextLight = styled.p`
+  font-size: ${(props) => props.theme.fontmd};
+  color: ${(props) => `rgba(${props.theme.bodyRgba}, 0.6)`};
+  align-self: flex-start;
+  margin: 1rem auto;
+  font-weight: 400;
+  width: 80%;
+`;
+
+const ButtonContainer = styled.div`
+  width: 80%;
+  margin: 1rem auto;
+  align-self: flex-start;
+`;
+
+const About = () => {
+  return (
+    <Section>
+      <Container>
+        <Box>
+          <Carousel />
+        </Box>
+        <Box>
+          <Title>Welcome to 0xButterfly</Title>
+          <SubText>
+            Authentic Wearable digital collectibles curation and showcasing
+            platform
+          </SubText>
+          <SubTextLight>
+            With more than 200+ hand drawn traits, each NFT is unique and
+            original, comes with a membership to an exclusive club access.
+          </SubTextLight>
+          <ButtonContainer>
+            <ThemeProvider theme={dark}>
+              <Button text="JOIN OUR DISCORD" link="#" />
+            </ThemeProvider>
+          </ButtonContainer>
+        </Box>
+      </Container>
+    </Section>
+  );
+};
+
+export default About;
